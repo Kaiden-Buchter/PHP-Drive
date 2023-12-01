c28f838534834814394848726a60673c3196292b
Kaiden Buchter <150194619+Kaiden-Buchter@users.noreply.github.com>
2023-12-01 17:50:48 -0500

    Add files via upload

---
+++ ChatApp/login.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+    if(isset($_SESSION['unique_id'])){ // If user is already logged in then redirect to users.php
+        header("location: users.php");
+    }
+?>
+<?php include_once "header.php"; ?>
+<body>
+    <div class="wrapper">
+        <section class="form login">
+            <header>Realtime Chat App</header>
+            <form action="#">
+                <div class="error-txt"></div>
+                <div class="field input">
+                    <label>Email Address</label>
+                    <input type="text" name="email" placeholder="Enter your email">
+                </div>
+                <div class="field input">
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="Enter your password">
+                    <i class="fas fa-eye"></i>
+                </div>
+                <div class="field button">
+                    <input type="submit" value="Continue to Chat">
+                </div>
+            </form>
+            <div class="link">Not signed up yet? <a href="index.php">Signup now</a></div>
+        </section>
+    </div>
+
+    <script src="./Javascript/Chat-App-pass-show-hide.js"></script>
+    <script src="./Javascript/login.js"></script>
+</body>
+</html>
